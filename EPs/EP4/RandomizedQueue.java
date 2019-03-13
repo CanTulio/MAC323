fc155e20ab5d7302022f3dd2bcf6bb36fb2754b4
Caio Andrade <caiotdandrade@gmail.com>
2019-03-12 22:25:57 -0300

    comeca randomized queue.

--- EPs/EP4/RandomizedQueue.java
+++ EPs/EP4/RandomizedQueue.java
@@ -35,3 +35,75 @@
     Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
 
 ****************************************************************/
+import java.lang.UnsupportedOperationException;
+import java.util.NoSuchElementException;
+import java.lang.IllegalArgumentException;
+
+import java.util.Random;
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    // construct an empty randomized queue
+    Item[] queue;
+    private int n;
+    private int topo;
+    public RandomizedQueue() {
+        this.queue = (Item[]) new Object[10];
+        this.n = 0;
+    }
+
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return n==0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return n;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        if (n < this.queue.length) {
+            this.queue[this.n] = item; // devo usar o this?
+            n++;
+        }
+        else {
+            resize(this.queue, 2*n);
+        }
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+
+    }
+
+    private Item[] resize(Item[] oldArray) {
+        int oldSize =  oldArray.length;
+        Item[] newArray = (Item[]) new Object(2*oldSize);
+        for (int i = 0; i < oldSize; i++) {
+            newArray[i] = oldArray[i];
+        }
+        return newArray;
+    }
+    // unit testing (required)
+    public static void main(String[] args) {
+        
+    }
+
+}
