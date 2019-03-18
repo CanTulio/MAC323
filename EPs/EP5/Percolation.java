c7d1da6e3542c00d5e56900521232d42cf9a5269
Caio Andrade <caiotdandrade@gmail.com>
2019-03-18 10:52:56 -0300

    Começa  EP05

---
+++ EPs/EP5/Percolation.java
@@ -0,0 +1,79 @@
+/****************************************************************
+    Nome: Caio Túlio de Deus Andrade
+    NUSP: 9797232
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitoras e colegas). Com exceção de material de MAC0323, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+
+    Exemplo:
+
+        A monitora me explicou que eu devia utilizar a função xyz().
+
+        O meu método xyz() foi baseada na descrição encontrada na 
+        página https://www.ime.usp.br/~pf/algoritmos/aulas/enumeracao.html.
+
+    Descrição de ajuda ou indicação de fonte:
+
+
+
+    Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
+
+****************************************************************/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        
+    }
+
+}
