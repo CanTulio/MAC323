f41a56e0ecef0cd56ce3515e5dadd380bd2c4a32
Caio Andrade <caiotdandrade@gmail.com>
2019-03-19 15:12:46 -0300

    Termina Percolation.java - Mudanças em estilo.
    * Adiciona comentários explicativos das variáveis.
    * Muda nome de variáveis para um nome mais auto-explicativo.
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- EPs/EP5/Percolation.java
+++ EPs/EP5/Percolation.java
@@ -45,25 +45,33 @@ import java.lang.IllegalArgumentException;
 
 public class Percolation {
 
-    // creates n-by-n grid, with all sites initially blocked
     private boolean[][] sitesOpen;
+        // Matrix that displays which sites are Open with a boolean value
     private int topSite;
+        // Virtual topsite used to track all sites that are connected to the top
     private int bottomSite;
+        // Virtual bottomsite to find out when the grid percolates
     private int openSites;
+        // Variable that mantains the number of open sites.
     private int n;
+        // Size of the grid
     private WeightedQuickUnionUF WQUF;
+        // Object from the Weighted Quick Union Find Data Structure.
 
 
+    // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
 
         if (n <= 0)
             throw new IllegalArgumentException();
-        WQUF = new WeightedQuickUnionUF(n*n+2); // permite armazenar o topsite e o bottomsite
+
+        WQUF = new WeightedQuickUnionUF(n*n+2);
+        sitesOpen = new boolean[n][n];
+        
         this.n = n;
         openSites = 0;
         topSite = n*n;
         bottomSite = n*n+1;
-        sitesOpen = new boolean[n][n];
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
@@ -80,7 +88,7 @@ public class Percolation {
 
         validatePosition(row, col);
         if( !isOpen(row, col) ) {
-            
+
             int linearIndex = toIndex(row, col);
             sitesOpen[row][col] = true;
 
