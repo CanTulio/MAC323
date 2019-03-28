a120285b5afbb98471dba461f5f2b776fd0559f7
Caio Andrade <caiotdandrade@gmail.com>
2019-03-28 09:12:21 -0300

    Começa EP6

---
+++ EPs/EP6/Board.java
@@ -0,0 +1,69 @@
+/****************************************************************
+    Nome: Caio Túlio de Deus Andrade
+    NUSP: 9797232
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitoras e colegas). Com exceção de material de MAC0323, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+    
+    Descrição de ajuda ou indicação de fonte:
+
+
+
+    Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
+
+****************************************************************/
+
+
+public class Board {
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles)
+                                           
+    // string representation of this board
+    public String toString()
+
+    // tile at (row, col) or 0 if blank
+    public int tileAt(int row, int col)
+
+    // board size n
+    public int size()
+
+    // number of tiles out of place
+    public int hamming()
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan()
+
+    // is this board the goal board?
+    public boolean isGoal()
+
+    // does this board equal y?
+    public boolean equals(Object y)
+
+    // all neighboring boards
+    public Iterable<Board> neighbors()
+
+    // is this board solvable?
+    public boolean isSolvable()
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+}
